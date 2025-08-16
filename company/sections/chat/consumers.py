20442d1d94c70962f33ca4ffb8454ec2414a6012
Salam Abdulsalam <abdulsalamabayomi300@gmail.com>
2025-08-16 04:01:28 +0100

    commit

--- company/sections/chat/consumers.py
+++ company/sections/chat/consumers.py
@@ -1,11 +1,11 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
-from .models import Message, GroupMessage
-from sections.groups.models import Group
+from django.apps import apps
 from asgiref.sync import sync_to_async
 
 User = get_user_model()
+
 
 class PrivateChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,30 +13,29 @@ class PrivateChatConsumer(AsyncWebsocketConsumer):
         self.receiver_username = self.scope["url_route"]["kwargs"]["receiver_username"]
         self.room_group_name = f"private_chat_{min(self.user.username, self.receiver_username)}_{max(self.user.username, self.receiver_username)}"
         self.chat_list_group = f"chat_list_{self.scope['user'].username}"
-        
-        await self.channel_layer.group_add(self.chat_list_group, self.channel_name)
 
+        await self.channel_layer.group_add(self.chat_list_group, self.channel_name)
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         await self.channel_layer.group_discard(self.chat_list_group, self.channel_name)
-
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         sender = self.user
         receiver = await sync_to_async(User.objects.get)(username=self.receiver_username)
-        picture = await self.get_profile_picture_url(sender.username)
-        total_count = await self.get_all_read_count(receiver)
-        count = await self.get_read(receiver)
 
+        Message = apps.get_model("chat", "Message")
+        GroupMessage = apps.get_model("chat", "GroupMessage")
+
+        picture = await self.get_profile_picture_url(sender.username)
+        total_count = await self.get_all_read_count(receiver, Message, GroupMessage)
+        count = await self.get_read(receiver, Message)
 
         message = await sync_to_async(Message.objects.create)(
-            sender=sender,
-            receiver=receiver,
-            content=data["message"]
+            sender=sender, receiver=receiver, content=data["message"]
         )
 
         await self.channel_layer.group_send(
@@ -51,39 +50,36 @@ class PrivateChatConsumer(AsyncWebsocketConsumer):
         )
 
         await self.channel_layer.group_send(
-            # f"chat_list_{receiver.username}",  # or sender.username depending on context
-            "chat_list",  # or sender.username depending on context
-
+            "chat_list",
             {
-                "type": "chat_list.update",  # 🔥 matches chat_list_update
+                "type": "chat_list.update",
                 "sender": sender.username,
                 "receiver": receiver.username,
                 "message": data["message"],
-                "timestamp": message.timestamp.strftime('%H:%M %p'),
-                "unread_count": count,  # must be calculated beforehand
-                "picture": picture, #if hasattr(receiver, 'profile_picture') else None,
+                "timestamp": message.timestamp.strftime("%H:%M %p"),
+                "unread_count": count,
+                "picture": picture,
                 "chat_count": total_count,
-            }
+            },
         )
 
     @sync_to_async
-    def get_read(self, receiver):
+    def get_read(self, receiver, Message):
         return Message.objects.filter(receiver=receiver, read=False).count()
-    
+
     @sync_to_async
     def get_profile_picture_url(self, username):
         user = User.objects.get(username=username)
-        return user.profile.profile_picture.url
+        return user.profile.profile_picture.url if user.profile.profile_picture else None
+
+    @sync_to_async
+    def get_all_read_count(self, user, Message, GroupMessage):
+        private_count = Message.objects.filter(receiver=user, read=False).count()
+        group_count = GroupMessage.objects.exclude(read_by=user).count()
+        return private_count + group_count
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
-
-    @sync_to_async
-    def get_all_read_count(self, user):
-        private_count = Message.objects.filter(receiver=user, read=False).count() # Count of unread messages from the other user
-        group_count = GroupMessage.objects.all().exclude(read_by=user).count()
-        total_count = private_count + group_count
-        return total_count
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -91,9 +87,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["slug"]
         self.room_group_name = f"chat_{self.room_name}"
         self.user = self.scope["user"]
-        
+
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -103,16 +98,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         message = data["message"]
 
+        Group = apps.get_model("groups", "Group")
+        Message = apps.get_model("chat", "Message")
+        GroupMessage = apps.get_model("chat", "GroupMessage")
 
-        # user = await self.get_user(username)
-        user  = self.user
-        room = await self.get_group(self.room_name)
-        new_message = await self.save_message(user, room, message)
-        picture  = await self.get_group_picture_url(self.room_name)
+        user = self.user
+        room = await self.get_group(self.room_name, Group)
+        new_message = await self.save_message(user, room, message, GroupMessage)
+        picture = await self.get_group_picture_url(self.room_name, Group)
         user_picture = await self.get_profile_picture_url(user.username)
-        # count = await self.get_read_count(room, user) 
-        total_count = await self.get_all_read_count(user)
-
+        total_count = await self.get_all_read_count(user, Message, GroupMessage)
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -121,12 +116,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "message": new_message["content"],
                 "sender": new_message["sender"],
                 "timestamp": new_message["timestamp"],
-                "picture": user_picture
-            }
+                "picture": user_picture,
+            },
         )
 
         await self.channel_layer.group_send(
-            f"group_chat_list",
+            "group_chat_list",
             {
                 "type": "group_list.update",
                 "message": new_message["content"],
@@ -136,61 +131,48 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "slug": room.slug,
                 "id": room.id,
                 "picture": picture,
-                # "unread_count": count,
                 "chat_count": total_count,
-            }
+            },
         )
 
     async def chat_message(self, event):
-        await self.send(text_data=json.dumps({
-            "message": event["message"],
-            "sender": event["sender"],
-            "timestamp": event["timestamp"],
-            "picture": event["picture"],
-        }))
-
+        await self.send(text_data=json.dumps(event))
 
     @sync_to_async
-    def get_all_read_count(self, user):
-        private_count = Message.objects.filter(receiver=user, read=False).count() # Count of unread messages from the other user
-        group_count = GroupMessage.objects.all().exclude(read_by=user).count()
-        total_count = private_count + group_count
-        return total_count
-    
-    @sync_to_async
-    def get_user(self, username):
-        return User.objects.get(username=username)
+    def get_all_read_count(self, user, Message, GroupMessage):
+        private_count = Message.objects.filter(receiver=user, read=False).count()
+        group_count = GroupMessage.objects.exclude(read_by=user).count()
+        return private_count + group_count
 
     @sync_to_async
     def get_profile_picture_url(self, username):
         user = User.objects.get(username=username)
-        return user.profile.profile_picture.url
-    
+        return user.profile.profile_picture.url if user.profile.profile_picture else None
+
     @sync_to_async
-    def get_group(self, room_name):
+    def get_group(self, room_name, Group):
         return Group.objects.get(slug=room_name)
 
     @sync_to_async
-    def get_group_picture_url(self, room_name):
+    def get_group_picture_url(self, room_name, Group):
         group = Group.objects.get(slug=room_name)
-        return group.profile_picture.url
+        return group.profile_picture.url if group.profile_picture else None
 
     @sync_to_async
-    def save_message(self, user, room, content):
+    def save_message(self, user, room, content, GroupMessage):
         message = GroupMessage.objects.create(sender=user, room=room, content=content)
-        return {"sender": user.username, "content": message.content, "room": message.room, "timestamp": str(message.timestamp.strftime("%H:%M, %b %d"))}
+        return {
+            "sender": user.username,
+            "content": message.content,
+            "timestamp": message.timestamp.strftime("%H:%M, %b %d"),
+        }
 
-    @sync_to_async
-    def get_previous_messages(self):
-        messages = GroupMessage.objects.filter(room=self.room_name).order_by("-timestamp")[:20]
-        return [{"sender": msg.sender, "content": msg.content, "timestamp": str(msg.timestamp)} for msg in messages]
 
 class ChatListConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
+        self.user = self.scope["user"]
         if self.user.is_authenticated:
-            # self.group_name = f'chat_list_{self.user.username}'
-            self.group_name = 'chat_list'
+            self.group_name = "chat_list"
             await self.channel_layer.group_add(self.group_name, self.channel_name)
             await self.accept()
         else:
@@ -201,22 +183,14 @@ class ChatListConsumer(AsyncWebsocketConsumer):
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def chat_list_update(self, event):
-        # This receives messages from group_send and forwards to WebSocket client
-        await self.send(text_data=json.dumps({
-            'sender': event['sender'],
-            'receiver': event['receiver'],
-            'message': event['message'],
-            'timestamp': event['timestamp'],
-            'unread_count': event['unread_count'],
-            'picture': event['picture'],
-            'chat_count': event['chat_count'],
-        }))
+        await self.send(text_data=json.dumps(event))
+
 
 class GroupListConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
+        self.user = self.scope["user"]
         if self.user.is_authenticated:
-            self.group_name = f'group_chat_list'
+            self.group_name = "group_chat_list"
             await self.channel_layer.group_add(self.group_name, self.channel_name)
             await self.accept()
         else:
@@ -227,26 +201,22 @@ class GroupListConsumer(AsyncWebsocketConsumer):
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def group_list_update(self, event):
+        Group = apps.get_model("groups", "Group")
+        GroupMessage = apps.get_model("chat", "GroupMessage")
+
         room_slug = event["slug"]
-        room = await self.get_group(room_slug)
-        count = await self.get_read_count(room, self.user)
+        room = await self.get_group(room_slug, Group)
+        count = await self.get_read_count(room, self.user, GroupMessage)
 
         await self.send(text_data=json.dumps({
-            "message": event["message"],
-            "sender": event["sender"],
-            "timestamp": event["timestamp"],
-            "id": event["id"],
-            "slug": event["slug"],
-            "name": event["name"],
-            "picture": event["picture"],
+            **event,
             "unread_count": count,
-            "chat_count": event['chat_count'],
         }))
 
     @sync_to_async
-    def get_group(self, room_name):
+    def get_group(self, room_name, Group):
         return Group.objects.get(slug=room_name)
 
     @sync_to_async
-    def get_read_count (self, room_name, user):
-        return GroupMessage.objects.filter(room=room_name).exclude(read_by=user).count()
+    def get_read_count(self, room, user, GroupMessage):
+        return GroupMessage.objects.filter(room=room).exclude(read_by=user).count()
