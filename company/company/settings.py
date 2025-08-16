df3d32c4b002c822ac6693bbce4fe8df4fe6199c
Salam Abdulsalam <abdulsalamabayomi300@gmail.com>
2025-08-16 01:48:38 +0100

    just a commit

--- company/company/settings.py
+++ company/company/settings.py
@@ -110,16 +110,16 @@ WSGI_APPLICATION = 'company.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# DATABASE_URL = os.getenv('DATABASE_URL')
 # DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
